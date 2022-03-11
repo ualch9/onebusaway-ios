e6c94857693c538b612a2a6184d5f9bf7127967a
Alan Chu <ualch9@gmail.com>
2022-03-11 12:00:31 -0800

    Replace parser with a custom NSError type
    Add URL key

--- OBAKitCore/Models/Helpers/DecodingErrorLocalizedParser.swift
+++ OBAKitCore/Models/Helpers/DecodingErrorLocalizedParser.swift
@@ -7,20 +7,45 @@
 
 import Foundation
 
-public class DecodingErrorLocalizedParser {
-    public static func parse(_ decodingError: DecodingError) -> String {
+public struct OBADecodingError: CustomNSError {
+    public static let errorDomain = "OBADecodingError"
+
+    public let errorCode = 500
+    public let errorUserInfo: [String: Any]
+
+    public init?(_ decodingError: DecodingError, url: URL? = nil) {
         switch decodingError {
         case .typeMismatch:
-            return parseTypeMismatch(decodingError)
+            self.errorUserInfo = OBADecodingError.userInfo(developerFacingDescription: OBADecodingError.parseTypeMismatch(decodingError), url: url)
         case .valueNotFound:
-            return decodingError.localizedDescription
+            self.errorUserInfo = OBADecodingError.userInfo(developerFacingDescription: OBADecodingError.parseValueNotFound(decodingError), url: url)
         case .keyNotFound:
-            return parseKeyNotFound(decodingError)
+            self.errorUserInfo = OBADecodingError.userInfo(developerFacingDescription: OBADecodingError.parseKeyNotFound(decodingError), url: url)
         case .dataCorrupted:
-            return decodingError.localizedDescription
+            return nil
         @unknown default:
-            return decodingError.localizedDescription
+            return nil
         }
+    }
+
+    fileprivate static func userInfo(
+        userFacingDescription: String = "500 The server for this transit region is having issues.",
+        developerFacingDescription: String,
+        url: URL? = nil
+    ) -> [String: Any] {
+        if let url = url {
+            return [
+                NSLocalizedDescriptionKey: userFacingDescription,
+                NSLocalizedFailureReasonErrorKey: developerFacingDescription,
+                NSURLErrorKey: url
+            ]
+        } else {
+            return [
+                NSLocalizedDescriptionKey: userFacingDescription,
+                NSLocalizedFailureReasonErrorKey: developerFacingDescription
+            ]
+        }
+
     }
 
     fileprivate static func parseCodingPath(_ codingPath: [CodingKey]) -> String {
@@ -28,16 +53,29 @@ public class DecodingErrorLocalizedParser {
     }
 
     fileprivate static func parseKeyNotFound(_ decodingError: DecodingError) -> String {
-        guard case let DecodingError.keyNotFound(codingKey, context) = decodingError else { return "" }
+        guard case let DecodingError.keyNotFound(codingKey, context) = decodingError else {
+            return decodingError.localizedDescription
+        }
 
         var fullCodingPath = context.codingPath
         fullCodingPath.append(codingKey)
 
-        return "Missing data: \(parseCodingPath(fullCodingPath))."
+        return "Missing required key \(parseCodingPath(fullCodingPath))."
+    }
+
+    fileprivate static func parseValueNotFound(_ decodingError: DecodingError) -> String {
+        guard case let DecodingError.valueNotFound(expectedType, context) = decodingError else {
+            return decodingError.localizedDescription
+        }
+
+        return "Missing required `\(String(describing: expectedType))` value for key \(parseCodingPath(context.codingPath)) (key exists, but either empty or NULL value)."
     }
 
     fileprivate static func parseTypeMismatch(_ decodingError: DecodingError) -> String {
-        guard case let DecodingError.typeMismatch(expectedType, context) = decodingError else { return "" }
+        guard case let DecodingError.typeMismatch(expectedType, context) = decodingError else {
+            return decodingError.localizedDescription
+        }
+
         return "Expected a \(String(describing: expectedType)) at \(parseCodingPath(context.codingPath))."
     }
 }
--- OBAKitTests/Modeling/REST Model Service Tests/DecodingErrorLocalizedParserTests.swift
+++ OBAKitTests/Modeling/REST Model Service Tests/DecodingErrorLocalizedParserTests.swift
@@ -8,6 +8,8 @@
 import XCTest
 import OBAKitCore
 
+/// This test case ensures that the relevant decoding error information is included in the displayable description.
+/// Primarily, indicating which specific JSON key has issues, which is causing decoding to fail. See `DecodingErrorLocalizedParser` for more details.
 class DecodingErrorLocalizedParserTests: XCTestCase {
     // MARK: - Test models
     fileprivate struct Vehicle: Codable {
@@ -29,61 +31,83 @@ class DecodingErrorLocalizedParserTests: XCTestCase {
         let contactInfo: ContactInfo
     }
 
-    // MARK: - Test inputs
-    fileprivate let missingOwnerContactEmail = """
-    {
-        "year": 2018,
-        "make": "Honda",
-        "model": "Accord",
-        "owner": {
-            "name": "Alan Chu",
-            "address": "Bellevue, WA",
-            "contactInfo": {
-                "phoneNumber": "4255555555"
-            }
-        }
-    }
-    """.data(using: .utf8)!
-
-    fileprivate let typeMismatchOwnerContactPhone = """
-    {
-        "year": 2018,
-        "make": "Honda",
-        "model": "Accord",
-        "owner": {
-            "name": "Alan Chu",
-            "address": "Bellevue, WA",
-            "contactInfo": {
-                "phoneNumber": 4255555555,
-                "email: "alan@example.com"
-            }
-        }
-    }
-    """.data(using: .utf8)!
-
-    fileprivate let nullValue = """
-    {
-        "year": 2018,
-        "make": "Honda",
-        "model": "Accord",
-        "owner": {
-            "name": "Alan Chu",
-            "address": "Bellevue, WA",
-            "contactInfo": {
-                "phoneNumber": null,
-                "email: "alan@example.com"
-            }
-        }
-    }
-    """.data(using: .utf8)!
+    fileprivate static let requestURL: URL = URL(string: "https://example.com/registered_owner?vin=ABCDEFG&id=1A2B3C")!
 
     // MARK: - Unit Testing
     let decoder = JSONDecoder()
 
     func testMissingValue() {
+        let missingOwnerContactEmail = """
+        {
+            "year": 2018,
+            "make": "Honda",
+            "model": "Accord",
+            "owner": {
+                "name": "Alan Chu",
+                "address": "Bellevue, WA",
+                "contactInfo": {
+                    "phoneNumber": "4255555555"
+                }
+            }
+        }
+        """.data(using: .utf8)!
+
         XCTAssertThrowsError(try decoder.decode(Vehicle.self, from: missingOwnerContactEmail), "") { error in
-            let localized = DecodingErrorLocalizedParser.parse(error as! DecodingError)
-            XCTAssertTrue(localized.contains("owner.contactInfo.email"), "MissingKey localized should contain the path \"owner.contactInfo.email\" somewhere in the string.")
+            let obaError: NSError = OBADecodingError(error as! DecodingError, url: Self.requestURL)! as NSError
+            XCTAssertFalse(obaError.localizedDescription.isEmpty, "Localized description should not be empty.")
+            XCTAssertTrue(obaError.localizedFailureReason!.contains("owner.contactInfo.email"), "MissingKey localized should contain the path \"owner.contactInfo.email\" somewhere in the string.")
+            XCTAssertEqual(obaError.userInfo[NSURLErrorKey] as! URL, Self.requestURL)
+        }
+    }
+
+    func testTypeMismatchValue() {
+        let typeMismatchOwnerContactPhone = """
+        {
+            "year": 2018,
+            "make": "Honda",
+            "model": "Accord",
+            "owner": {
+                "name": "Alan Chu",
+                "address": "Bellevue, WA",
+                "contactInfo": {
+                    "phoneNumber": 4255555555,
+                    "email": "alan@example.com"
+                }
+            }
+        }
+        """.data(using: .utf8)!
+
+        XCTAssertThrowsError(try decoder.decode(Vehicle.self, from: typeMismatchOwnerContactPhone), "") { error in
+            let obaError: NSError = OBADecodingError(error as! DecodingError, url: Self.requestURL)! as NSError
+            XCTAssertFalse(obaError.localizedDescription.isEmpty, "Localized description should not be empty.")
+            XCTAssertTrue(obaError.localizedFailureReason!.contains("owner.contactInfo.phoneNumber"), "TypeMismatch localized should contain the path \"owner.contactInfo.phoneNumber\" somewhere in the string.")
+            XCTAssertEqual(obaError.userInfo[NSURLErrorKey] as! URL, Self.requestURL)
+        }
+    }
+
+    func testUnexpectedNullValueOrValueNotFound() {
+        let nullValue = """
+        {
+            "year": 2018,
+            "make": "Honda",
+            "model": "Accord",
+            "owner": {
+                "name": "Alan Chu",
+                "address": "Bellevue, WA",
+                "contactInfo": {
+                    "phoneNumber": null,
+                    "email": "alan@example.com"
+                }
+            }
+        }
+        """.data(using: .utf8)!
+
+        XCTAssertThrowsError(try decoder.decode(Vehicle.self, from: nullValue), "") { error in
+            let obaError: NSError = OBADecodingError(error as! DecodingError, url: Self.requestURL)! as NSError
+            XCTAssertFalse(obaError.localizedDescription.isEmpty, "Localized description should not be empty.")
+            XCTAssertTrue(obaError.localizedFailureReason!.contains("owner.contactInfo.phoneNumber"), "TypeMismatch localized should contain the path \"owner.contactInfo.phoneNumber\" somewhere in the string.")
+            XCTAssertTrue(obaError.localizedFailureReason!.lowercased().contains("string"), "TypeMismatch localized should contain the expected type, \"String\", somewhere in the string.")
+            XCTAssertEqual(obaError.userInfo[NSURLErrorKey] as! URL, Self.requestURL)
         }
     }
 }
